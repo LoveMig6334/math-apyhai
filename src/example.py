c6519ee22c06ea77c368d281e99e5bcd9a047016
Thatt Bunnag <tom.tom.thanet@gmail.com>
2026-05-04 14:29:10 +0700

    Remove Python version specification and project metadata; update execution counts in Jupyter notebook; add example script for basic functionality

---
+++ src/example.py
@@ -0,0 +1,18 @@
+def main() -> None:
+    print("Hello world")
+
+
+def menu() -> str:
+    while True:
+        try:
+            print("")
+            menu = int(input())
+        except ValueErros:
+            pass
+
+
+
+if __name__ == "__main__":
+    main()
+
+
